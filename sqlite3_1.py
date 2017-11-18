b21ad3d48472a5a0fe7c24a2e93b252dc1658779
James Dindi <dindijjames@gmail.com>
2017-11-18 13:50:39 +0300

    Add and update sqlite

--- sqlite3_1.py
+++ sqlite3_1.py
@@ -2,11 +2,12 @@ import sqlite3
 import time
 import datetime
 import random
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib import style
-style.use('fivethirtyeight')
 
+style.use('fivethirtyeight')
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
@@ -32,8 +33,32 @@ def read_from_db():
     c.execute("SELECT * FROM stuffToPlot WHERE unix < 1510884480 AND value >= 3")
     # data = c.fetchall()
     for row in c.fetchall():
-        print row
+        print(row)
 
+def graph_data():
+    c.execute("SELECT unix, value FROM stuffToPlot")
+    dates = []
+    values = []
+    for row in c.fetchall():
+        # print(row[0])
+        # print(datetime.datetime.fromtimestamp(row[0]))
+        dates.append(datetime.datetime.fromtimestamp(row[0]))
+        values.append(row[1])
+    plt.plot_date(dates, values, '-')
+    plt.show()
+
+def del_and_update():
+    c.execute("SELECT * FROM stuffToPlot")
+    [print(row) for row in c.fetchall()]
+    print("-----------------------------------------------------")
+    # c.execute("UPDATE stuffToPlot SET value = 99 WHERE value = 8")
+    # conn.commit()
+    # c.execute("SELECT * FROM stuffToPlot")
+    # [print(row) for row in c.fetchall()]
+
+    c.execute("DELETE FROM stuffToPlot WHERE value = 99")
+    c.execute("SELECT * FROM stuffToPlot")
+    [print(row) for row in c.fetchall()]
 
 
 # create_table()
@@ -42,6 +67,8 @@ def read_from_db():
 #     dynamic_data_entry()
 #     time.sleep(1)
 # read_from_db()
+# graph_data()
+del_and_update()
 
 c.close()
 conn.close()
